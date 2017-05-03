32f9f27e8c2aaf143aa19d0bc85abae841914bbf
maxxwell131 <maxxwell131@yahoo.com>
2017-05-03 18:07:34 +0300

    Initial Commit

---
+++ ShoeConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoeConverter
+//
+//  Created by admin on 5/3/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoeConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoeConverter
+//
+//  Created by admin on 5/3/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
